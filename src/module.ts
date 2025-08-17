ab6a421a84e1ef9964e9658c417bdf3c41118f3f
Isaac Aeshliman <iaeshliman@gmail.com>
2025-08-16 23:23:40 -0500

    refactor: import configuration module

--- src/module.ts
+++ src/module.ts
@@ -5,9 +5,14 @@
 // NestJS Libraries
 import { Module } from '@nestjs/common'
 
+// Modules
+import { ConfigurationModule } from './configuration/module'
+
 /**
  * Module
  */
 
-@Module({})
+@Module({
+    imports: [ConfigurationModule],
+})
 export class RootModule {}
